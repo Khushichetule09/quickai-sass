9dea50c0502ffe9c8257288a82e7f94c6a7e8c1e
Khushi Chetule <khushichetule2@gmail.com>
2025-12-25 14:42:41 +0530

    update the navbar file

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,39 @@
 import React from 'react'
 import { assets } from '../assets/assets'
-import { Navigate, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { ArrowRight } from 'lucide-react'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
-    const navigate = useNavigate()
-  return (
-    <div className='fixed z-5 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32'>
-        <img src={assets.logo} alt="logo" className='w-32 sm:w-44 cursor-pointer' onClick={() => navigate('/')} />
+  const navigate = useNavigate()
+  const { user, isLoaded } = useUser()
+  const { openSignIn } = useClerk()
 
-        <button className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'>Get started <ArrowRight className='w-4 h-4'/></button>
+  // 🔴 VERY IMPORTANT (Clerk loading)
+  if (!isLoaded) return null
+
+  return (
+    <div className='fixed top-0 z-50 w-full bg-white/80 backdrop-blur-md flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32'>
       
+      <img
+        src={assets.logo}
+        alt="logo"
+        className='w-32 sm:w-44 cursor-pointer'
+        onClick={() => navigate('/')}
+      />
+
+      {
+        user
+          ? <UserButton />
+          : (
+            <button
+              onClick={() => openSignIn()}
+              className='flex items-center gap-2 rounded-full text-sm bg-primary text-white px-10 py-2.5'
+            >
+              Get started <ArrowRight className='w-4 h-4'/>
+            </button>
+          )
+      }
     </div>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,18 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import { ClerkProvider } from '@clerk/clerk-react'
 
-const root = createRoot(document.getElementById('root'))
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
+createRoot(document.getElementById('root')).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>
 )
-
