ebeeb28070eebada0d51911c42fa328b2b43d97e
Khushi Chetule <khushichetule2@gmail.com>
2025-12-25 13:42:17 +0530

    Implement responsive navbar on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import React from 'react'
+import Navbar from "../components/Navbar";
+
+const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   )
 }
+
+export default Home
